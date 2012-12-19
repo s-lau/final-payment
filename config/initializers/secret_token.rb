936ee7036d363e2cf980c51c35291bc369bd0095
Hendrik Bergunde <hbergunde@gmx.de>
2012-12-19 19:25:26 +0100

    rails new chargeback

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chargeback::Application.config.secret_token = '2bd3ec0c80221e14fa64dab7972833c81ff326cd1a817be85de39326b9fbda90312d249241a29908b3e8fa90bd9117943ad36cf213aa32cd3147081367d22877'

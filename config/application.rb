1b8a858c59ae3bcf3ea0433143800a0c5547833e
Hendrik Bergunde <hbergunde@gmx.de>
2012-12-23 23:16:05 +0100

    make factory_girl create factories instead of models

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,9 @@ module Chargeback
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.fixture_replacement :factory_girl
+    end
   end
 end

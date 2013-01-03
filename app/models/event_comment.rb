4c319113c62decdf349a913ad98ae6f6ce1d4f2f
Hendrik Bergunde <hbergunde@gmx.de>
2013-01-03 23:44:42 +0100

    make further attributes accessable + validation

--- app/models/event_comment.rb
+++ app/models/event_comment.rb
@@ -1,5 +1,7 @@
 class EventComment < ActiveRecord::Base
-  attr_accessible :text
+  attr_accessible :text, :event, :author
   belongs_to :author, foreign_key: "user_id", class_name: "User"
   belongs_to :event, foreign_key: "event_uuid"
+
+  validates_presence_of :text
 end

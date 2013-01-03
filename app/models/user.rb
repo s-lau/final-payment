3eb6332cf1aef51de294970724d92efbd9328c1e
Hendrik Bergunde <hbergunde@gmx.de>
2013-01-04 00:44:45 +0100

    add uniqueness validation to user_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
 
   validates_presence_of :username
+  validates_uniqueness_of :username
 
   has_many :events, foreign_key: "owner"
 end

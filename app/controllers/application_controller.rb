143e609d2135476800f645fc3c3e1d70c675edb2
Stephan Lau <lau@mi.fu-berlin.de>
2013-01-16 13:41:35 +0100

    return to last "authentication required" path on sign in/up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
     end
     
     def after_sign_in_path_for(resource)
-      events_path
+      session[:user_return_to] || events_path
     end
     
     def after_sign_out_path_for(resource)

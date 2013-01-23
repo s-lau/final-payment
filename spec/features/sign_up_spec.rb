b9d7a249c6b92d2d66c45e73067d5d522740277f
Hendrik Bergunde <hbergunde@gmx.de>
2013-01-23 02:15:34 +0100

    refine spec

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,13 +1,12 @@
 # encoding: utf-8
 require 'spec_helper'
-require 'net/http'
 
 feature '13 B: Registration' do
 
   scenario 'Successful Registration' do
     -> {
       sign_up_with 'user@example.com', '123456', 'user'
-    }.should change(User, :count).by 1
+    }.should change(ActionMailer::Base.deliveries,:size).by(1)
     User.count.should be 1
     User.first.confirmed?.should be false
     User.first.confirm!
